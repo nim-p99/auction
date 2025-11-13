6bc9850a9de12e407b2a03f03a00b58560b26368
laluke1 <lalukeadams@gmail.com>
2025-11-14 02:43:39 +1100

    linked browse to auction with join to item

--- browse.php
+++ browse.php
@@ -6,6 +6,12 @@
 
 <h2 class="my-3">Browse LISTINGS</h2>
 
+<!-- Navigation -->
+<?php
+$filter_cat = $_GET['cat'] ?? 'fill'; // default to 'all' categories
+$sort_by = $_GET['sort'] ?? 'date_asc'; // default to 'date_asc'
+?>
+
 
 <div id="searchSpecs">
 <!-- When this form is submitted, this PHP page is what processes it.
@@ -102,34 +108,38 @@
 
 <!-- TODO: If result set is empty, print an informative message. Otherwise... -->
 
-<ul class="list-group">
 
-<!-- TODO: Use a while loop to print a list item for each auction listing
-     retrieved from the query -->
+<!----------------------------------------------------------------------------
+                          Listing auctions
+----------------------------------------------------------------------------->
 
-<?php
-  // Demonstration of what listings will look like using dummy data.
-  $item_id = "87021";
-  $title = "Dummy title";
-  $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget rutrum ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus feugiat, ipsum vel egestas elementum, sem mi vestibulum eros, et facilisis dui nisi eget metus. In non elit felis. Ut lacus sem, pulvinar ultricies pretium sed, viverra ac sapien. Vivamus condimentum aliquam rutrum. Phasellus iaculis faucibus pellentesque. Sed sem urna, maximus vitae cursus id, malesuada nec lectus. Vestibulum scelerisque vulputate elit ut laoreet. Praesent vitae orci sed metus varius posuere sagittis non mi.";
-  $current_price = 30;
-  $num_bids = 1;
-  $end_date = new DateTime('2020-09-16T11:00:00');
-  
-  // This uses a function defined in utilities.php
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
-  
-  $item_id = "516";
-  $title = "Different title";
-  $description = "Very short description.";
-  $current_price = 13.50;
-  $num_bids = 3;
-  $end_date = new DateTime('2020-11-02T00:00:00');
-  
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
-?>
 
-</ul>
+<div class="list-container">
+<?php 
+  global $connection;
+
+  $final_query = "SELECT * from auction AS a 
+  JOIN item AS i 
+  ON a.item_id = i.item_id  ";
+  $auctions_to_list = mysqli_query($connection, $final_query);
+
+//loop through each item and display it
+  while ($row = mysqli_fetch_assoc($auctions_to_list)) : ?>
+      <div class="list-group">
+        <?php 
+        $item_id = $row['item_id'];
+        $title = $row['title'];
+        $description = $row['description'];
+        $end_date = $row['end_date_time']; // Placeholder end date
+        $current_price = $row['current_price']; 
+        $num_bids =$row['num_bids'];
+
+        //using the print listing function from utilities.php
+        print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date) ?>
+      </div>
+      
+  <?php endwhile; ?>
+  </div>
 
 <!-- Pagination for results listings -->
 <nav aria-label="Search results pages" class="mt-5">
